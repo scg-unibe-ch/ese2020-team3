a2e45f118a12523a516248b74e230e837877a512
Oceanic13 <68353591+Oceanic13@users.noreply.github.com>
2020-10-04 13:25:33 +0200

    Added input forms in user registration component

--- frontend/src/app/user-registration/user-registration.component.ts
+++ frontend/src/app/user-registration/user-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-user-registration',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegistrationComponent implements OnInit {
 
-  constructor() { }
+  firstName = '';
+  lastName = '';
+  userName = '';
+  email = '';
+  password = '';
+
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
   }
 
+  register(): void {
+      window.alert('Register');
+  }
+
+  /**
+   * Provides Feedback for the typed in password. The password must consist of
+   * small, capital, special and at least 7 characters.
+   */
+  passwordFeedback(): string {
+    var nonSpecialCharacters = /^[A-Za-z0-9 ]+$/;
+    var capitalLetters = /[A-Z]/;
+    var smallLetters = /[a-z]/
+
+    if (!capitalLetters.test(this.password))
+      return 'Must include Capital Letters!'
+    else if (!smallLetters.test(this.password))
+      return 'Must include Small Letters!'
+    else if (nonSpecialCharacters.test(this.password))
+      return 'Must include Special Characters!';
+    else if (this.password.length<7)
+      return 'Must include 7 Characters (' + this.password.length + '/7)';
+    return ''
+  }
+
+  /**
+   * Provides Feedback for the typed in email.
+   */
+  emailFeedback(): string {
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(this.email) ? '' : 'No valid email!';
+  }
+
+  userNameFeedback(): string {
+   return 'Bad Username';
+  }
+
+  /**
+   * Returns whether someone is currently logged in to hide this component.
+   */
+  loggedIn(): boolean {
+    return !!(localStorage.getItem('userToken'));
+  }
 }
