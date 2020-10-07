5f232d230053755fab97a731bddcd4c9b41b1ba1
Can Kilic <can.kilic@students.unibe.ch>
2020-10-07 16:15:16 +0200

    function in registration-form: "confirmpassword" has to match "password"

--- frontend/src/app/user-registration/user-registration.component.ts
+++ frontend/src/app/user-registration/user-registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-user-registration',
@@ -8,36 +9,38 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class UserRegistrationComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {
+  }
+
   registrationForm: FormGroup;
+
   ngOnInit(): void {
 
     this.registrationForm = this.formBuilder.group({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.pattern(
-        '^(?=.{7,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\\W).*$'
-      )
-      ]),
-      confirmpassword: new FormControl('', Validators.required),
-      firstname: new FormControl('', Validators.required),
-      lastname: new FormControl('', Validators.required),
-      username: new FormControl('', Validators.required),
-      gender: new FormControl(''),
-      telephonenumber: new FormControl(''),
-      street: new FormControl(''),
-      pincode: new FormControl(''),
-      city: new FormControl(''),
-      country: new FormControl('')
-    }
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.pattern(
+          '^(?=.{7,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\\W).*$'
+        )
+        ]),
+        confirmpassword: new FormControl('', Validators.required),
+        firstname: new FormControl('', Validators.required),
+        lastname: new FormControl('', Validators.required),
+        username: new FormControl('', Validators.required),
+        gender: new FormControl(''),
+        telephonenumber: new FormControl(''),
+        street: new FormControl(''),
+        pincode: new FormControl(''),
+        city: new FormControl(''),
+        country: new FormControl('')
+      }, {validators: passwordMatchValidator}
     );
   }
-  validPassword(): boolean {
-    return this.registrationForm.get('password').invalid
-      ? false
-      : this.registrationForm.get('password').value ===
-      this.registrationForm.get('passwordconfirm').value;
-  }
-
-
 }
+
+  export const passwordMatchValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
+    if (formGroup.get('password').value === formGroup.get('confirmpassword').value)
+      return null;
+    else
+      return {passwordMismatch: true};
+  };
 
